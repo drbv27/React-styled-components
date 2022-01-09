983a4eed6517fdc9d77d15cb1264201178fe8a13
Diego <drbv27@gmail.com>
2022-01-09 07:53:46 -0500

    Adapting properties and composition

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import styled from "styled-components";
 
 const P = styled.p`
   font-size: 24px;
-  color: red;
+  color: purple;
 `;
+
+const Content = styled.div`
+  padding: 20px 25px;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => (props.primary ? "purple" : "white")};
+  color: ${(props) => (props.primary ? "white" : "purple")};
+  padding: 10px 15px;
+  border: solid 2px purple;
+  border-radius: 4px;
+`;
+
 const App = () => {
   return (
-    <div>
+    <Content>
       <P>Hola soy un parrafo!</P>
-    </div>
+      <Button>Enviar</Button>
+      <Button primary>Enviar</Button>
+      <Button primary={true}>Enviar</Button>
+    </Content>
   );
 };
 
